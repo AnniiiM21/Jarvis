81da3543052e5f73a20bde87bdee1fefe5e272de
thunderblast21 <aniruddha212004@gmail.com>
2025-05-08 14:53:33 +0530

    deploy changes done

--- Jarvis/src/components/Main/Main.jsx
+++ Jarvis/src/components/Main/Main.jsx
@@ -68,8 +68,8 @@ const Main = () => {
               <div className="search-box">
                 <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
                 <div>
-                  <img src={assets.gallery_icon} alt="" />
-                  <img src={assets.mic_icon} alt="" />
+                  {/*<img src={assets.gallery_icon} alt="" />
+                  <img src={assets.mic_icon} alt="" />*/}
                   {input?<img onClick={()=>onSent()} src={assets.send_icon} alt="" />:null}
                 </div>
               </div>
